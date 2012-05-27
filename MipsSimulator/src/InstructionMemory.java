48ac2b7c2485263d58c644d863fa9e2b642638f3
ndomar.14 <ndomar.14@17349f25-2ac4-f15d-cabf-50f2efa9f3e0>
2012-05-27 18:15:53 +0000

    Reverted to older version.

--- MipsSimulator/src/InstructionMemory.java
+++ MipsSimulator/src/InstructionMemory.java
@@ -1,7 +1,5 @@
 
-import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import javax.security.sasl.AuthorizeCallback;
@@ -14,7 +12,6 @@ public class InstructionMemory {
     boolean done;
     HashMap<Integer, String> instructions = new HashMap<Integer, String>();
     HashMap<String, Integer> labels = new HashMap<String, Integer>();
-    HashMap<String, Integer> data = new HashMap<String, Integer>();
     Organizer controller = new Organizer();
     Wire ALURegister = new Wire("Instruction Memory", "Register File", "", 0);
     Wire ALU_MUXRegister = new Wire("Instruction Memory", "Register File", "", 0);
@@ -29,9 +26,9 @@ public class InstructionMemory {
     PC_MUX pcMUX;
     DataMemory memory;
     Wire ALU_MUXSecondInput = new Wire("Instrucion Memory", "ALU_MUX", "", 0);
+    Wire controllerWire = new Wire("Instruction Memory", "Controller", "", 0);
 
     public InstructionMemory(ArrayList<String> file) {
-        boolean containsOrg = false;
         this.memory = new DataMemory(this.regMUX, this);
         this.wiresLog.add(new ArrayList<ArrayList<String>>());
         this.pcMUX = new PC_MUX(this, this.PC_MUXOut);
@@ -46,67 +43,26 @@ public class InstructionMemory {
         }
         System.out.println("Starting address is " + startingAddress);
         System.out.println("File size is " + file.size());
-        boolean dataSeg = false;
         for (int i = 1, j = startingAddress; i <= file.size(); i++, j += 4) {
             String line = file.get(i - 1);
-            // AUTHOR YAHIA
-            if (line.contains(".data")) {
-            	System.out.println("IN THE DATA SEGMENT");
-            	dataSeg = true;
-            	continue;
-            }
-            else if (line.contains(".text")) {
-            	System.out.println("IN THE TEXT SEGMENT");
-            	dataSeg = false;
-            	continue;
-            }
-            
-            if (dataSeg) {
-            	
-                String[] split = line.split(": ",2);
-                System.out.println(Arrays.toString(split));	
-                if (split.length < 3 && split[1].startsWith(".word")) {
-                	int value = 0;
-                	String[] split2 = split[1].split(" ");
-                	System.out.println(Arrays.toString(split2));
-                	if (split2[1].startsWith("0x"))
-                	{
-                		// I am really sorry for the following line :'( 
-                		// it's the simplest. It's evil I know :'(, :'(
-                		BigInteger bi = new BigInteger(split2[1].substring(2), 16);
-                		value = bi.intValue();
-                	}else 
-                	{
-                		value = Integer.parseInt(split2[1]);
-                	}
-                	System.out.println("adding " + split[0] + " => " + value);
-                	data.put(split[0], value);
-                }
-                
-            } else if (line.contains(":")) {
-                String[] split = line.split("[: ]");
+            if (line.contains(":")) {
+                String[] split = line.split(": ");
                 System.out.println("adding labels " + split[0] + " to " + split[1] + " line (" + j + ")");
                 labels.put(split[0], j);
                 instructions.put(j, split[1]);
-                
-            // AUTHOR YAHIA END 
             } else {
-                if(file.get(i - 1).contains("ORG"))
-                {
+                if (file.get(i - 1).contains("ORG")) {
                     String[] splittedInstruction = file.get(i - 1).split(" ");
                     startingAddress = Integer.parseInt(splittedInstruction[1]);
-                    containsOrg = true;
-                    j -=4;
+                    j -= 4;
+                } else {
+                    instructions.put(j, file.get(i - 1));
+                    System.out.println("putting in slot " + j + " instruction " + file.get(i - 1));
                 }
-                else
-                {
-                                   instructions.put(j, file.get(i - 1));
-                System.out.println("putting in slot " + j + " instruction " + file.get(i - 1));
-                }
-             
-           
-                
-        
+
+
+
+
             }
         }
         controller.setInstances(alu, aluMUX, registerFile, this, regMUX, memory, this.pcMUX);
@@ -204,8 +160,11 @@ public class InstructionMemory {
         String op = arr[0];
         this.wiresLog.get(COMMANDS_COUNTER).add(new ArrayList<String>());
         this.wiresLog.get(COMMANDS_COUNTER).add(new ArrayList<String>());
-        this.controller.setControls(arr[0]);
+        this.controller.setControllerWire(controllerWire);
+
         if (op.equalsIgnoreCase("add")) {
+            this.controllerWire.setData(1);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             WriteRegister.setData(this.registerFile.registers.get(arr[1]));
             ALU_MUXRegister.setDestinationRegister(arr[3]);
@@ -217,6 +176,8 @@ public class InstructionMemory {
             //this.registerFile.setFirstOperandDestination(arr[2]);
             //this.registerFile.setSecondOperandDestination(arr[3]);
         } else if (op.equalsIgnoreCase("addi")) {
+            this.controllerWire.setData(2);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             ALURegister.setData(this.registerFile.registers.get(arr[2]));
             ALURegister.setDestinationRegister(arr[2]);
@@ -224,6 +185,8 @@ public class InstructionMemory {
             this.aluMUX.setInput(1, this.ALU_MUXSecondInput);
             this.registerFile.setFirstOperandDestination();
         } else if (op.equalsIgnoreCase("sub")) {
+            this.controllerWire.setData(3);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             WriteRegister.setData(this.registerFile.registers.get(arr[1]));
             ALU_MUXRegister.setDestinationRegister(arr[3]);
@@ -233,16 +196,22 @@ public class InstructionMemory {
             this.registerFile.setFirstOperandDestination();
             this.registerFile.setSecondOperandDestination();
         } else if (op.equalsIgnoreCase("lw")) {
+            this.controllerWire.setData(4);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             ALURegister.setDestinationRegister(arr[2]);
             this.registerFile.setFirstOperandDestination();
         } else if (op.equalsIgnoreCase("sw")) {
+            this.controllerWire.setData(5);
+            this.controller.setControls();
             ALURegister.setDestinationRegister(arr[2]);
             ALU_MUXRegister.setDestinationRegister(arr[1]);
             this.registerFile.setFirstOperandDestination();
 
             registerFile.forward();
         } else if (op.equalsIgnoreCase("sll")) {
+            this.controllerWire.setData(6);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             ALURegister.setData(this.registerFile.registers.get(arr[2]));
             ALURegister.setDestinationRegister(arr[2]);
@@ -250,6 +219,8 @@ public class InstructionMemory {
             this.aluMUX.setInput(1, this.ALU_MUXSecondInput);
             this.registerFile.setFirstOperandDestination();
         } else if (op.equalsIgnoreCase("srl")) {
+            this.controllerWire.setData(7);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             ALURegister.setData(this.registerFile.registers.get(arr[2]));
             ALURegister.setDestinationRegister(arr[2]);
@@ -257,6 +228,8 @@ public class InstructionMemory {
             this.aluMUX.setInput(1, this.ALU_MUXSecondInput);
             this.registerFile.setFirstOperandDestination();
         } else if (op.equalsIgnoreCase("and")) {
+            this.controllerWire.setData(8);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             WriteRegister.setData(this.registerFile.registers.get(arr[1]));
             ALU_MUXRegister.setDestinationRegister(arr[3]);
@@ -266,6 +239,8 @@ public class InstructionMemory {
             this.registerFile.setFirstOperandDestination();
             this.registerFile.setSecondOperandDestination();
         } else if (op.equalsIgnoreCase("andi")) {
+            this.controllerWire.setData(9);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             ALURegister.setData(this.registerFile.registers.get(arr[2]));
             ALURegister.setDestinationRegister(arr[2]);
@@ -273,6 +248,8 @@ public class InstructionMemory {
             this.aluMUX.setInput(1, this.ALU_MUXSecondInput);
             this.registerFile.setFirstOperandDestination();
         } else if (op.equalsIgnoreCase("or")) {
+            this.controllerWire.setData(10);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             WriteRegister.setData(this.registerFile.registers.get(arr[1]));
             ALU_MUXRegister.setDestinationRegister(arr[3]);
@@ -282,6 +259,8 @@ public class InstructionMemory {
             this.registerFile.setFirstOperandDestination();
             this.registerFile.setSecondOperandDestination();
         } else if (op.equalsIgnoreCase("ori")) {
+            this.controllerWire.setData(11);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             ALURegister.setData(this.registerFile.registers.get(arr[2]));
             ALURegister.setDestinationRegister(arr[2]);
@@ -289,6 +268,8 @@ public class InstructionMemory {
             this.aluMUX.setInput(1, this.ALU_MUXSecondInput);
             this.registerFile.setFirstOperandDestination();
         } else if (op.equalsIgnoreCase("nor")) {
+            this.controllerWire.setData(12);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             WriteRegister.setData(this.registerFile.registers.get(arr[1]));
             ALU_MUXRegister.setDestinationRegister(arr[3]);
@@ -298,6 +279,8 @@ public class InstructionMemory {
             this.registerFile.setFirstOperandDestination();
             this.registerFile.setSecondOperandDestination();
         } else if (op.equalsIgnoreCase("beq")) {
+            this.controllerWire.setData(13);
+            this.controller.setControls();
             ALU_MUXRegister.setDestinationRegister(arr[2]);
             ALU_MUXRegister.setData(this.registerFile.registers.get(arr[2]));
             ALURegister.setData(this.registerFile.registers.get(arr[1]));
@@ -308,6 +291,8 @@ public class InstructionMemory {
             this.jump.setData(getJumpValue(arr[3]));
             this.pcMUX.setInput(1, jump);
         } else if (op.equalsIgnoreCase("bne")) {
+            this.controllerWire.setData(14);
+            this.controller.setControls();
             ALU_MUXRegister.setDestinationRegister(arr[2]);
             ALU_MUXRegister.setData(this.registerFile.registers.get(arr[2]));
             ALURegister.setData(this.registerFile.registers.get(arr[1]));
@@ -317,22 +302,30 @@ public class InstructionMemory {
             this.jump.setData(getJumpValue(arr[3]));
             this.pcMUX.setInput(1, jump);
         } else if (op.equalsIgnoreCase("j")) {
+            this.controllerWire.setData(15);
+            this.controller.setControls();
             this.jump.setData(getJumpValue(arr[1]));
             this.pcMUX.setInput(1, jump);
         } else if (op.equalsIgnoreCase("jal")) {
+            this.controllerWire.setData(16);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister("$ra");
             WriteRegister.setData(this.PC_MUXOut.getData());
             System.out.println("pc mux out is " + this.PC_MUXOut.getData());
             ALURegister.setData(this.PC_MUXOut.getData() + 4);
-            this.ALU_MUXRegister.setData(0);;
+            this.ALU_MUXRegister.setData(0);
             this.registerFile.setFirstOperandDirectDestination();
             this.jump.setData(getJumpValue(arr[1]));
             this.pcMUX.setInput(1, jump);
 
         } else if (op.equalsIgnoreCase("jr")) {
+            this.controllerWire.setData(17);
+            this.controller.setControls();
             this.jump.setData(this.registerFile.registers.get(arr[1]));
             this.pcMUX.setInput(1, jump);
         } else if (op.equalsIgnoreCase("slt")) {
+            this.controllerWire.setData(18);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             WriteRegister.setData(this.registerFile.registers.get(arr[1]));
             ALU_MUXRegister.setDestinationRegister(arr[3]);
@@ -342,6 +335,8 @@ public class InstructionMemory {
             this.registerFile.setFirstOperandDestination();
             this.registerFile.setSecondOperandDestination();
         } else if (op.equalsIgnoreCase("slti")) {
+            this.controllerWire.setData(19);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             ALURegister.setData(this.registerFile.registers.get(arr[2]));
             ALURegister.setDestinationRegister(arr[2]);
@@ -349,6 +344,8 @@ public class InstructionMemory {
             this.aluMUX.setInput(1, this.ALU_MUXSecondInput);
             this.registerFile.setFirstOperandDestination();
         } else if (op.equalsIgnoreCase("sltu")) {
+            this.controllerWire.setData(20);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             WriteRegister.setData(this.registerFile.registers.get(arr[1]));
             ALU_MUXRegister.setDestinationRegister(arr[3]);
@@ -358,18 +355,20 @@ public class InstructionMemory {
             this.registerFile.setFirstOperandDestination();
             this.registerFile.setSecondOperandDestination();
         } else if (op.equalsIgnoreCase("sltui")) {
+            this.controllerWire.setData(21);
+            this.controller.setControls();
             WriteRegister.setDestinationRegister(arr[1]);
             ALURegister.setData(this.registerFile.registers.get(arr[2]));
             ALURegister.setDestinationRegister(arr[2]);
             this.ALU_MUXSecondInput.setData(Integer.parseInt(arr[3]));
             this.aluMUX.setInput(1, this.ALU_MUXSecondInput);
             this.registerFile.setFirstOperandDestination();
-        } 
+        }
         if (!(arr[0].equalsIgnoreCase("sw") || arr[0].equalsIgnoreCase("lw") || arr[0].equalsIgnoreCase("jal"))) {
             this.aluMUX.forward();
         }
-       
-                    this.wiresLog.get(COMMANDS_COUNTER).get(1).add("ALU FIRST INPUT: " + this.ALURegister.toString());
+
+        this.wiresLog.get(COMMANDS_COUNTER).get(1).add("ALU FIRST INPUT: " + this.ALURegister.toString());
         this.wiresLog.get(COMMANDS_COUNTER).get(1).add("ALU MUX INPUT: " + this.ALU_MUXRegister.toString());
         this.wiresLog.get(COMMANDS_COUNTER).get(1).add("WRITE REGISTER: " + this.WriteRegister.toString());
         this.wiresLog.get(COMMANDS_COUNTER).get(1).add("JUMP: " + this.jump.toString());
@@ -378,7 +377,7 @@ public class InstructionMemory {
 
         this.alu.doOperation();
         this.pcMUX.forward();
-        
+
 
 
     }
@@ -418,38 +417,13 @@ public class InstructionMemory {
         System.out.println("The program used " + cycles + " cycles");
     }
 
-    public static void main(String[] abbas) {
+    /**
+     * The method uses the factorial logic to add numbers to each other instead
+     * of multiplying FACT(4) = 4 + FACT(3) FACT(0) = 1 FACT(1) = 1
+     */
+    public static void testRecursiveCode() {
         ArrayList<String> file = new ArrayList<String>();
-        /*
-         * file.add("sll $t0 $t0 1"); file.add("add $t0 $t0 $t0"); file.add("sub
-         * $t1 $t0 $t0");
-         *
-         * file.add("or $t0 $t0 $t0"); file.add("and $t0 $t0 $t0"); file.add("sw
-         * $t0 12($t0)"); file.add("lw $t1 12($t0)"); file.add("addi $t1 $t1
-         * 1"); file.add("sltui $t2 $t1 -3"); // file.add("and $t1 $t0 $t0");
-         */
-        //file.add("add $t0 $t0 $t0");
-        //  file.add("bne $t1 $t0 8");
-        // file.add("end");
-        //file.add("addi $t1 $t0 2");
-        //file.add("addi $t0 $t0 1");
-        // the following instruction won't be executed, we used branch dumbass!
-        // file.add("ZA3: add $t2 $zero $t0");
-        // file.add("jal 4");
-        // file.add("addi $t1 $t1 1");
-        //  file.add("end");
-        // file.add("addi $t0 $zero 0");
-        //file.add("slti $t2 $t0 10");
-        //file.add("beq $t2 $zero 22");
-        //file.add("add $s0 $s0 $s1");
-        //file.add("addi $t0 $t0 1");
-        //file.add("jal 4");
-        //file.add(" end");
-        file.add(".data");
-        file.add("a: .word 0xFF");
-        file.add("b: .word 5");
-        
-        file.add(".text");
+        file.add("ORG 100");
         file.add("jal FACT");
         file.add("end");
         file.add("FACT: addi $sp $sp -8");
@@ -467,17 +441,54 @@ public class InstructionMemory {
         file.add("addi $sp $sp 8");
         file.add("add $v0 $a0 $v0");
         file.add("jr $ra");
-        file.add("addi $zero $t1 0");
-        // file.add("lw $t0 0($t0)");
-
-
-
         InstructionMemory is = new InstructionMemory(file);
-        System.out.println(is.registerFile.registers.get("$zero"));
-        System.out.println(is.registerFile.registers.get("$t1"));
-        System.out.println(is.registerFile.registers.get("$t2"));
-        System.out.println(is.registerFile.registers.get("$ra"));
-        System.out.println(is.registerFile.registers.get("$v0"));
-        //is.printWiresLog();
+        is.printWiresLog();
+
+    }
+
+    /**
+     * The method tests the functionality of arithmetic instructions.
+     */
+    public static void testArithmeticInstructions() {
+        ArrayList<String> file = new ArrayList<String>();
+        file.add("sll $t0 $t0 2");
+        file.add("srl $t1 $t0 2");
+        file.add("and $t2 $t1 $t0");
+        file.add("andi $t3 $t0 2");
+        file.add("or $t4 $t3 $t2");
+        file.add("ori $t5 $t4 5");
+        file.add("nor $t6 $t5 $t4");
+        file.add("slt $s0 $t0 $t4");
+        file.add("sltui $s1 $t0 2");
+        file.add("sltu $s2 $s1 $s0");
+        InstructionMemory is = new InstructionMemory(file);
+        is.printWiresLog();
+       
+    }
+
+    /**
+     * The Method keeps subtracting one from t1, until $t1 becomes 0
+     */
+    public static void testSubtractor() {
+        ArrayList<String> file = new ArrayList<String>();
+        file.add("addi $t0 $zero 1");
+        file.add("addi $t1 $zero 5");
+        file.add("CHECK: bne $t1 $zero LOOP");
+        file.add("LOOP: sub $t1 $t1 $t0");
+        file.add("beq $t1 $zero END");
+        file.add("j CHECK");
+        file.add("END: end");
+        InstructionMemory is = new InstructionMemory(file);
+        is.printWiresLog();
+
+
+    }
+
+    public static void main(String[] abbas) {
+
+        testRecursiveCode();
+        testArithmeticInstructions();
+        testSubtractor();
+
     }
 }
